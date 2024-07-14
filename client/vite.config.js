fb2bb5e053cc751770760e438615f68044838cfc
sajjad nazari dev <sajadnazari269@gmail.com>
2024-07-14 19:51:10 +0330

    update structure project

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.', // تعیین دایرکتوری ریشه
-  publicDir: 'public', // تعیین دایرکتوری عمومی
-  build: {
-    outDir: 'dist',
-  },
   server: {
-    host: '127.0.0.1',
-    port: 5173,
     open: true, // باز کردن مرورگر به صورت خودکار
   },
-});
+  build: {
+    outDir: '../dist', // مسیر خروجی برای دایرکتوری اصلی
+  }
+})
